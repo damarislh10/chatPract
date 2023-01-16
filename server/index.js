3ba1ea8e9f0e32c3fb1141fd4237db7a92505c8d
damarislh10 <cortezhernandezdamaris@gmail.com>
2023-01-16 16:56:53 -0500

    inicio servidor

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server Started on Port ${process.env.PORT}`);
 })
